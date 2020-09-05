821118e8923a612957c1e9d7ddb92e7e556298c2
bonino97 <juanbonino97@gmail.com>
2020-09-05 18:14:40 -0300

    Add Monitoring Module

--- API-LemonBooster-v2/Helpers/PaginatedResult.js
+++ API-LemonBooster-v2/Helpers/PaginatedResult.js
@@ -71,3 +71,39 @@ exports.PaginatedResponseCodesByScope = async (model, page, limit, scope, filter
         throw e;
     }
 }
+
+exports.PaginatedMonitoringResults = async (model, page, limit, filter) => {
+    try{
+        const results = {}  
+
+        if(model) {
+            const startIndex = (page - 1) * limit;
+            const endIndex = page * limit;
+            results.results = model.slice(startIndex, endIndex);
+
+            queryModel = model.filter((data) => data.toLowerCase()).filter((data) => data.toLowerCase().indexOf(filter.toLowerCase()) > -1);
+            results.results = queryModel.slice(startIndex, endIndex);
+            
+            if(endIndex < queryModel.length)        
+            results.nextPage = {
+                page: page+1,
+                limit: limit
+            }
+        
+            if(startIndex > 0){
+                results.previousPage = {
+                    page: page-1,
+                    limit: limit
+                }
+            }
+
+            results.totalPages = Math.ceil(queryModel.length/limit);
+            results.actualPage = page;
+
+            return results;
+        }
+
+    } catch (e){
+        throw e;
+    }
+}
--- API-LemonBooster-v2/Routes/MonitoringRoutes.js
+++ API-LemonBooster-v2/Routes/MonitoringRoutes.js
@@ -3,3 +3,8 @@ const app = express();
 
 //CONTROLLERS
 
+const MonitoringController = require('../Controllers/MonitoringController');
+
+app.get('/:url', MonitoringController.GetMonitoringByDate);
+
+module.exports = app;
