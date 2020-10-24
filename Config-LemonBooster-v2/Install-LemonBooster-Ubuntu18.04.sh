1312ef634334167c45af663f5835f9a6b80c5458
Juan Cruz <juanbonino97@gmail.com>
2020-10-24 19:55:32 -0300

    Fix ubuntu 18.04 sh

--- Config-LemonBooster-v2/Install-LemonBooster-Ubuntu18.04.sh
+++ Config-LemonBooster-v2/Install-LemonBooster-Ubuntu18.04.sh
@@ -39,15 +39,15 @@ echo "Installing GNUPG..."
 sudo apt-get install -y gnupg
 
 echo "Importing MongoDB public GPG Key..."
-wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
 
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 
 echo "Reloading packages..."
-sudo apt-get -y update
+sudo apt -y update
 
 echo "Installing MongoDB..."
-sudo apt-get install -y mongodb-org
+sudo apt install -y mongodb-org
 
 echo "mongodb-org hold" | sudo dpkg --set-selections
 echo "mongodb-org-server hold" | sudo dpkg --set-selections
@@ -59,7 +59,7 @@ echo "Reloading services..."
 sudo systemctl daemon-reload
 
 echo "Start MongoDB"
-sudo systemctl start mongod
+sudo systemctl start mongod.service
 
 echo "Verifying if MongoDB is installed..."
 sudo systemctl status mongod
