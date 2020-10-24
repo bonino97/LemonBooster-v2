95101a7cdafe1cde959b131fc7d65fc337eab066
Juan Cruz <juanbonino97@gmail.com>
2020-10-24 18:59:33 -0300

    Reload packages

--- Config-LemonBooster-v2/Install-LemonBooster-Ubuntu18.04.sh
+++ Config-LemonBooster-v2/Install-LemonBooster-Ubuntu18.04.sh
@@ -43,6 +43,9 @@ wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add 
 
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 
+echo "Reloading packages..."
+sudo apt-get -y update
+
 echo "Installing MongoDB..."
 sudo apt-get install -y mongodb-org
 
