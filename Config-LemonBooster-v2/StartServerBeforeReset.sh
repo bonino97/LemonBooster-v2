4da63711fd4eccae2195a5a29a9f0e9c0b331eaa
Juan Cruz <juanbonino97@gmail.com>
2020-10-25 18:03:17 -0300

    Fix all

--- Config-LemonBooster-v2/StartServerBeforeReset.sh
+++ Config-LemonBooster-v2/StartServerBeforeReset.sh
@@ -3,6 +3,8 @@
 sudo systemctl daemon-reload
 sudo systemctl start mongod
 sudo systemctl status mongod
+pm2 delete 0
 cd ~/tools/LemonBooster-v2/API-LemonBooster-v2
 npm install --save dotenv
 pm2 start ~/tools/LemonBooster-v2/API-LemonBooster-v2/Index.js
+pm2 logs
