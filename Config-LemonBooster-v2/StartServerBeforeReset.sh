4672fdb0579c77b0e87cd24b041447465c1bbc96
Juan Cruz <juanbonino97@gmail.com>
2020-10-25 18:02:15 -0300

    Fix start server before reset

--- Config-LemonBooster-v2/StartServerBeforeReset.sh
+++ Config-LemonBooster-v2/StartServerBeforeReset.sh
@@ -3,4 +3,6 @@
 sudo systemctl daemon-reload
 sudo systemctl start mongod
 sudo systemctl status mongod
+cd ~/tools/LemonBooster-v2/API-LemonBooster-v2
+npm install --save dotenv
 pm2 start ~/tools/LemonBooster-v2/API-LemonBooster-v2/Index.js
