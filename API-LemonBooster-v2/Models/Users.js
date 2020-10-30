35a0bad683a3b8ab8e75827197e1f37913d88d63
Juan Cruz <juanbonino97@gmail.com>
2020-10-30 14:41:03 -0300

    Add users model

--- API-LemonBooster-v2/Models/Users.js
+++ API-LemonBooster-v2/Models/Users.js
@@ -1,8 +1,5 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-const slug = require('slug');
-const shortId = require('shortid');
-const bcrypt = require('bcrypt');
 mongoose.pluralize(null);
 
 const UserSchema = new mongoose.Schema({ 
@@ -13,38 +10,14 @@ const UserSchema = new mongoose.Schema({
         unique: true,
         lowercase: true
     },
-    Username:{
-        type: String,
-        required: 'Username required.'
-    },
-    Password:{
-        type: String, 
-        required: 'Password required.',
-        trim: true
-    },
-
+    GitToken: String,
+    TelegramToken: String,
+    TelegramChatId: String,
+    VirusTotalApiKey: String,
+    FacebookApiKey: String,
+    SecurityTrailsApiKey: String,
     CreatedAt: Date,
 
 });
-
-//Metodo para Hashear los Passwords
-UserSchema.pre('save', async function (next) {
-    //Si el password ya esta hasheado, no hacemos nada.
-    if(!this.isModified('Password')) {
-        return next(); //Detener la ejecucion, y continuar con el siguiente middleware
-    }
-    const Hash = await bcrypt.hash(this.Password, 10);
-    this.Password = Hash; 
-
-    next();
-});
-
-//Autenticar Usuarios
-UserSchema.methods = {
-    ComparePassword: function(Password){
-        return bcrypt.compareSync(Password, this.Password);
-    }
-}
-
 
 module.exports = mongoose.model('User', UserSchema);
